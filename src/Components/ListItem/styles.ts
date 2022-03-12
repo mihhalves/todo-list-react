680442fc79dd98f02f856e0a2c805a72bc9b8eb3
michelle <fotosmihalves@gmail.com>
2022-03-12 19:50:25 -0300

    🎨  add styles to ListItem comp

---
+++ src/Components/ListItem/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display:flex;
+    background: #20212c;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+
+    Input {
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+    }
+
+    label {
+        color #ccc;
+        
+    }
+`;
