f5f73faa46f422b55e3bf6b524a6cb18ee328c7c
michelle <fotosmihalves@gmail.com>
2022-03-13 18:51:16 -0300

    💫 add image

---
+++ src/Components/AddArea/index.tsx
@@ -0,0 +1,10 @@
+import * as C from "./styles";
+
+export const AddArea = () => {
+  return (
+    <C.Container>
+      <div className="image">➕</div>
+      <input type="text" placeholder="add new task" />
+    </C.Container>
+  );
+};
